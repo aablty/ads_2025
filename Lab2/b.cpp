732876cbb63f7b475200aca0e6581210a9939ad1
aablty <acrouple@yandex.ru>
2025-09-19 14:46:29 +0500

    lab2_b: re: by Linked List

--- Lab2/b.cpp
+++ Lab2/b.cpp
@@ -2,26 +2,88 @@
 #include <string>
 using namespace std;
 
-void fill(string *&els, int n)
+struct Node
 {
-    for (int i = 0; i < n; i++)
+    string data;
+    Node *next;
+    Node(const string &d) : data(d), next(nullptr) {}
+};
+
+class LinkedList
+{
+    Node *head;
+    Node *tail;
+    int size;
+
+public:
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    void push_back(const string &val)
     {
-        cin >> els[i];
+        Node *node = new Node(val);
+        if (!head)
+        {
+            head = tail = node;
+        }
+        else
+        {
+            tail->next = node;
+            tail = node;
+        }
+        size++;
     }
-}
+
+    void shift(int k)
+    {
+        if (!head || size == 0 || k % size == 0)
+            return;
+        k = k % size;
+        Node *curr = head;
+        for (int i = 1; i < k; ++i)
+        {
+            curr = curr->next;
+        }
+        Node *new_head = curr->next;
+        curr->next = nullptr;
+        tail->next = head;
+        head = new_head;
+        tail = curr;
+    }
+
+    void print()
+    {
+        Node *curr = head;
+        while (curr)
+        {
+            cout << curr->data << " ";
+            curr = curr->next;
+        }
+    }
+
+    ~LinkedList()
+    {
+        Node *curr = head;
+        while (curr)
+        {
+            Node *tmp = curr;
+            curr = curr->next;
+            delete tmp;
+        }
+    }
+};
 
 int main()
 {
     int N, K;
     cin >> N >> K;
-
-    string *els = new string[N];
-    fill(els, N);
-
-    for (int i = 0; i < N; i++)
+    LinkedList list;
+    for (int i = 0; i < N; ++i)
     {
-        cout << els[(i + K) % N] << " ";
+        string s;
+        cin >> s;
+        list.push_back(s);
     }
-
+    list.shift(K);
+    list.print();
     return 0;
 }
